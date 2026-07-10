5341e3b65b9ebad9a47cc950af0acd29597324d9
Asadalvi979 <asadalvi979@gmail.com>
2026-07-10 07:22:32 +0500

    Revert "Add seed_data management command, fix start.sh error handling"
    This reverts commit 007c7e55e6143e69fcac5e1f21f29de0d12659fd.

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 cd CampNect_Backend
-python manage.py migrate --noinput
-python manage.py seed_data
-python manage.py collectstatic --noinput
+python manage.py migrate --noinput || true
+python manage.py collectstatic --noinput || true
 gunicorn CampNect_Backend.wsgi --bind 0.0.0.0:$PORT
